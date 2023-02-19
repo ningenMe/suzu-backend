435fe69804a27df3902f943773e77e7c072db64f
ningenMe <amohkuru@gmail.com>
2023-02-20 03:32:20 +0900

    [fix]

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
+use std::sync::Arc;
+
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
 use tonic::{transport::Server, Request, Response, Status};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
+use suzu::blog_service_server::{BlogService, BlogServiceServer};
+use suzu::{Blog, GetBlogResponse};
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
+pub mod suzu {
+    tonic::include_proto!("suzu");
 }
 
 #[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct MyBlogService {}
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
-        &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloReply>, Status> {
-        println!("Got a request: {:?}", request);
+impl BlogService for MyBlogService {
+    type GetStream = ReceiverStream<Result<GetBlogResponse, Status>>;
 
-        let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into(),
-        };
+    async fn get(&self, _request: Request<()>) -> Result<Response<Self::GetStream>, Status> {
+        let (tx, rx) = mpsc::channel(4);
+        let response_vec:Vec<GetBlogResponse> = vec![
+            GetBlogResponse{
+                blog: None
+            },
+            GetBlogResponse{
+                blog: None
+            },
+        ];
 
-        Ok(Response::new(reply))
+        println!("hoge1");
+
+        tokio::spawn(async move {
+            for response in &response_vec[..]{
+                println!("hoge2");
+                tx.send(Ok(response.clone())).await.unwrap();
+            }
+        });
+        println!("hoge3");
+        Ok(Response::new( ReceiverStream::new(rx)))
     }
 }
 
@@ -30,10 +46,10 @@ impl Greeter for MyGreeter {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::default();
+    let blog_service = MyBlogService::default();
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(BlogServiceServer::new(blog_service))
         .serve(addr)
         .await?;
 
